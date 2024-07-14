454eac6976460ab317367469098cdcf0c7d07721
김지수 <tttghost@gmail.com>
2024-07-14 23:09:45 +0900

    폴더구조 정립

---
+++ Assets/test.cs
@@ -0,0 +1,29 @@
+using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour
+{
+    public Image img;
+    public Renderer rd;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AA().Forget();
+    }
+    
+    
+    async UniTask AA()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            img.color = Color.white * i * 0.1f;
+            rd.material.color = Color.white * i * 0.1f;
+            await Task.Delay(100);
+        }
+    }
+}

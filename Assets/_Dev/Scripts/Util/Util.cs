5de8ffe331f5ad5e3a7c29f4a88aeb574519c1cb
김지수 <tttghost@gmail.com>
2024-07-31 02:09:34 +0900

    util기능추가 - ui element extensionMethod

--- Assets/_Dev/Scripts/Util/Util.cs
+++ Assets/_Dev/Scripts/Util/Util.cs
@@ -781,5 +781,33 @@ public static partial class Util
     {
         return Mathf.Lerp(toA, toB, Mathf.InverseLerp(fromA, fromB, val));
     }
+
+    #region setdata - 컴포넌트에 부가기능 추가
+
+    public static Button SetData(this Button btn, Action action, eAudioClips eAudioClips = eAudioClips.effect_click, float volumeScale = 1f)
+    {
+        btn.onClick.RemoveAllListeners();
+        btn.onClick.AddListener(() =>
+        {
+            action?.Invoke();
+            SoundManager.instance.PlayEffect(eAudioClips, volumeScale);
+        });
+
+        return btn;
+    }
+
+    public static Toggle SetData(this Toggle tog, Action action = null, Action<bool> actionBool = null, eAudioClips eAudioClips = eAudioClips.effect_click, float volumeScale = 1f)
+    {
+        tog.onValueChanged.RemoveAllListeners();
+        tog.onValueChanged.AddListener((b) =>
+        {
+            action?.Invoke();
+            actionBool?.Invoke(b);
+            SoundManager.instance.PlayEffect(eAudioClips, volumeScale);
+        });
+
+        return tog;
+    }
+    #endregion
 }
 

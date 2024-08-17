cffa627b25e4db156fa2e5905fa715e7a7d8f546
김지수 <tttghost@gmail.com>
2024-08-17 23:52:24 +0900

    panel_Wheel 추가 (바퀴 캐릭터 변경패널)

---
+++ Assets/_Dev/Scripts/UI/panel/panel_Wheel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class panel_Wheel : panel_Base
+{
+    private Button btn_Wheel;
+    private Image img_Question;
+
+    protected override void CacheComponent()
+    {
+        base.CacheComponent();
+        btn_Wheel = gameObject.Search<Button>(nameof(btn_Wheel)).SetData(GoldController.Instance.OnClick_GetGold);
+        img_Question = gameObject.Search<Image>(nameof(img_Question));
+    }
+}

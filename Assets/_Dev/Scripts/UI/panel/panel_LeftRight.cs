2fe15f57205da1c82122789329e5f0bf3191199b
김지수 <tttghost@gmail.com>
2024-08-17 23:51:14 +0900

    스크립트 추가 - panel_LeftRight (왼쪽 오른쪽 누르며 panel_Wheel에 기능 전달) panel_LevelUp (레벨업관련 기능 담당)

---
+++ Assets/_Dev/Scripts/UI/panel/panel_LeftRight.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel_LeftRight : panel_Base
+{
+    protected override void CacheComponent()
+    {
+        base.CacheComponent();
+    }
+}
---
+++ Assets/_Dev/Scripts/UI/panel/panel_LevelUp.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel_LevelUp : panel_Base
+{
+    protected override void CacheComponent()
+    {
+        base.CacheComponent();
+    }
+}

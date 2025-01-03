463f7afac1d5fa5c66192b3e03a972c4e5479d5a
tttghost <tttghost@gmail.com>
2025-01-03 15:37:51 +0900

    사운드매니저 리팩토링 - Awake에서 AudioSource,AudioClip초기화 하지 않고 외부에서 초기화(주입) 코드삭제

--- Assets/_Dev/Scripts/Manager/Sounds/SoundManager.cs
+++ Assets/_Dev/Scripts/Manager/Sounds/SoundManager.cs
@@ -3,14 +3,9 @@ using WheelMaker.Manager.Interfaces;
 
 namespace WheelMaker.Manager.Sounds
 {
-    public class SoundManager : MonoBehaviour, IAudioControl
     {
-        [field: SerializeField] public AudioSource AudioSource { get; set; }
-        [field: SerializeField] public AudioClip AudioClip { get; set; }
 
-        private void Awake()
         {
-            AudioSource.clip = AudioClip;
         }
 
         public void Play()

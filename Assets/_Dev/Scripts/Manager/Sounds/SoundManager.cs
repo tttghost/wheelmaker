061da34bec5c9767f46c41ecdac90b74e30518b7
tttghost <tttghost@gmail.com>
2025-01-03 15:38:09 +0900

    사운드매니저 리팩토링 - Awake에서 AudioSource,AudioClip초기화 하지 않고 외부에서 초기화(주입) 코드추가

--- Assets/_Dev/Scripts/Manager/Sounds/SoundManager.cs
+++ Assets/_Dev/Scripts/Manager/Sounds/SoundManager.cs
@@ -3,9 +3,32 @@ using WheelMaker.Manager.Interfaces;
 
 namespace WheelMaker.Manager.Sounds
 {
+    public class SoundManager : MonoBehaviour, IAudioControl, IVolumeControl
     {
+        [field: SerializeField] public AudioSource AudioSource { get; private set; }
 
+        private float volume;
+        public float Volume 
+        { 
+            get => volume;
+            set 
+            {
+                volume = value;
+                Debug.Log($"Set volume : {volume.ToString("0.00")}");
+                if(AudioSource != null )
+                {
+                    AudioSource.volume = value;
+                }
+            } 
+        }
+
+        public void Initialize(AudioSource audioSource, AudioClip audioClip)
         {
+            AudioSource = audioSource;
+            if (audioSource != null)
+            {
+                audioSource.clip = audioClip;
+            }
         }
 
         public void Play()
@@ -18,6 +41,18 @@ namespace WheelMaker.Manager.Sounds
         {
             Debug.Log("Stop");
             AudioSource.Stop();
+        }
+
+        public void Pause()
+        {
+            Debug.Log("Pause");
+            AudioSource.Pause();
+        }
+
+        public void UnPause()
+        {
+            Debug.Log("UnPause");
+            AudioSource.UnPause();
         }
     }
 }
